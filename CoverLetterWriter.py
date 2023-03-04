4262bdaa7a6936e33add07a9a7a709f59c06e044
paulwhite93 <91712154+paulwhite93@users.noreply.github.com>
2023-03-03 18:12:59 -0800

    Containerized the Job Sections

--- CoverLetterWriter.py
+++ CoverLetterWriter.py
@@ -1,170 +1,219 @@
-import tkinter
+from tkinter import *
+from tkinter import filedialog
 import openai
 
+
 openai.api_key = "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" #must be set before it will function
-
-#open ai startup 
-
-#Store new text
-global jobDesc
-jobDesc = ""
-global job_title
-job_title = ""
-global company_name
-company_name = ""
+resumeFile = "resume_master.txt"
 global resume
 resume = ""
-global newResume
-newResume = ""
+#open ai startup 
 
-#function definitions    
-def sendData(data):
-    #sends data to openai
-    #sets the outputtxt to the output data from openai
-    print(data)
-    chat_completion = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages=[{"role":"user","content":data}])
-    return chat_completion.choices[0].message.content
+class Job:
+    def __init__(self,jobDescription,title,c_name) -> None:
+        self.jobDesc = jobDescription
+        self.job_title = title
+        self.company_name = c_name
+        self.jobResume = resume
+        self.open_window()
 
-def refine_resume():
-    #open ai resume refinement
-    #open a new window with refined resume returned by openAI and prompt user if theyd like to save to a file
-    def send():
-        global resume
-        resume = inputtxt.get("1.0",'end-1c')
-        prompt = "please personalize my resume for this: " + job_title +" " + company_name + "\n Here is the job description: " + jobDesc + "\n And here is my resume: " + resume
-        outputtxt.insert(tkinter.END,sendData(prompt))
-    def save():
-        write_resume_to_file(outputtxt.get("1.0",'end-1c'))
-        window.destroy()
+    def open_window(self):
+        self.JobWindow = Toplevel(GUIWindow)
+        self.JobWindow.geometry("1280x720")
+        self.JobWindow.title(self.company_name)
+
+        Label(self.JobWindow, text='Job Title:').grid(row=0)
+        Label(self.JobWindow, text='Company Name:').grid(row=1)
+        Label(self.JobWindow, text='Job Description:').grid(row=2)
+        Label(self.JobWindow, text='Resume:').grid(row=2, column = 20, sticky=E)
+
+        #setJobDescriptionButton = Button(JobWindow, text="Set Job Description", command = self.set_job_description)
+        #setJobDescriptionButton.grid(row = 3, column = 0)
+
+        refineResumeButton = Button(self.JobWindow, text="Refine Resume", command = self.refine_resume)
+        refineResumeButton.grid(row = 0, column = 3)
+
+        createCoverLetter = Button(self.JobWindow, text="Create Cover Letter", command = self.create_cover_letter)
+        createCoverLetter.grid(row = 1, column = 3)
+        self.set_job_desc()
+        self.set_company_name()
+        self.set_job_title()
+        self.set_resume()
+
+    #function definitions    
+    def sendData(self,data):
+        #sends data to openai
+        #sets the outputtxt to the output data from openai
+        print(data)
+        chat_completion = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages=[{"role":"user","content":data}])
+        return chat_completion.choices[0].message.content
+
+    def refine_resume(self):
+        #open ai resume refinement
+        #open a new window with refined resume returned by openAI and prompt user if theyd like to save to a file
+        def send():
+            prompt = "please personalize my resume for this: " + self.job_title +" " + self.company_name + "\n Here is the job description: " + self.jobDesc + "\n And here is my resume: " + self.jobResume
+            resume_outputtxt.insert(END,self.sendData(prompt))
+
+        def save():
+            self.write_resume_to_file(resume_outputtxt.get("1.0",'end-1c'))
+            window.destroy()
+            return
+        
+        window = Toplevel(self.JobWindow)
+        window.title("Refining Resume")
+        window.geometry("1280x720")
+
+        outputlabel = LabelFrame(window, text= "Refined Resume")
+        outputlabel.grid(row=1,column=0)
+
+        resume_outputtxt = Text(outputlabel, height=40, width=120)
+        resume_outputtxt.grid(row = 1)
+
+        saveResumeButton = Button(outputlabel, text = "Save Resume", command = save).grid(row = 2)
+        send()
+    
+
+    def create_cover_letter(self):
+        #create cover letter using open ai and job description
+        #open a new window with cover letter returned by openAI and prompt user if theyd like to save to a file
+        def send():
+            prompt = "please write a personalized cover letter for this " + self.job_title + " at " + self.company_name + "\n Here is the job description: " + self.jobDesc + "\n And here is my resume: " + resume
+            outputtxt.insert(END,self.sendData(prompt))
+        def save():
+            self.write_cover_letter_to_file(outputtxt.get("1.0",'end-1c'))
+            window.destroy()
+        
+        window = Toplevel(self.JobWindow)
+        window.title("Creating Cover Letter")
+        window.geometry("1280x720")
+
+        submitButton = Button(window, text = "Create Cover Letter", command = send)
+        submitButton.pack()
+
+        outputlabel = Label(window, text= "Cover Letter")
+        outputlabel.pack()
+
+        outputtxt = Text(window, height=40, width=120)
+        outputtxt.pack()
+
+        saveCoverLetterButton = Button(window, text = "Save Cover Letter", command = save)
+        saveCoverLetterButton.pack()
         return
-    
-    window = tkinter.Toplevel(GUIWindow)
-    window.title("Refining Resume")
-    window.geometry("1280x720")
 
-    inputlabel = tkinter.Label(window, text= "Please paste your resume here:")
-    inputlabel.pack()
-
-    inputtxt = tkinter.Text(window, height=10, width=50)
-    inputtxt.pack()
-
-    submitButton = tkinter.Button(window, text = "Refine", command = send)
-    submitButton.pack()
-
-    outputlabel = tkinter.Label(window, text= "Refined Resume")
-    outputlabel.pack()
-
-    outputtxt = tkinter.Text(window, height=10, width=50)
-    outputtxt.pack()
-
-    saveResumeButton = tkinter.Button(window, text = "Save Resume", command = save)
-    saveResumeButton.pack()
-    
-    
-
-    return
-def set_job_description():
-    #store job description for use in open ai commands
-    def saveInfo():
-        global jobDesc
-        jobDesc = jobdescriptiontxt.get("1.0",'end-1c')
-        global job_title
-        job_title = e1.get()
-        global company_name
-        company_name = e2.get()
-        set_company_name()
-        set_job_title()
-        set_job_desc()
-        window.destroy()
+    def write_resume_to_file(self,data):
+        #write resume to a file
+        #if were saving new resume 
+        global newResume
+        newResume = data
+        file = filedialog.asksaveasfile(mode='w',initialfile = self.company_name + "_resume", defaultextension=".txt",)
+        file.write(data)
+        file.close()
         return
+    def write_cover_letter_to_file(self,data):
+        #write cover letter to a file
+        file = filedialog.asksaveasfile(mode='w',initialfile = self.company_name + "_coverLetter", defaultextension=".txt",)
+        file.write(data)
+        file.close()
 
-    window = tkinter.Toplevel(GUIWindow)
-    window.title("Setting Job Description and Company Name")
-    window.geometry("1280x720")
+    def set_job_title(self):
+        Label(self.JobWindow, text= self.job_title).grid(row = 0, column = 1)
+    def set_company_name(self):
+        Label(self.JobWindow, text= self.company_name).grid(row = 1, column = 1)
+    def set_job_desc(self):
+        jobDesc_text = Text(self.JobWindow,height=30, width=80)
+        jobDesc_text.grid(row = 3, column = 0,columnspan = 20, rowspan = 6,)
+        jobDesc_text.insert(END,self.jobDesc)
+    def set_resume(self):
+        jobResume_text = Text(self.JobWindow,height=30, width=80)
+        jobResume_text.grid(row = 3, column = 20,columnspan = 20, rowspan = 6,)
+        jobResume_text.insert(END,self.jobResume)
 
-    tkinter.Label(window, text='Job Title:').grid(row=0)
-    tkinter.Label(window, text='Company Name').grid(row=1)
-    e1 = tkinter.Entry(window)
-    e2 = tkinter.Entry(window)
-    e1.grid(row=0, column=1)
-    e2.grid(row=1, column=1)
 
-    jobdescriptionlabel = tkinter.Label(window, text= "Please paste your job description here:")
-    jobdescriptionlabel.grid(row = 2)
-    jobdescriptiontxt = tkinter.Text(window, height=10, width=50)
-    jobdescriptiontxt.grid(row = 3)
-
-    submitButton = tkinter.Button(window, text = "Save Information", command = saveInfo)
-    submitButton.grid(row = 4)
     
-    return
-def create_cover_letter():
-    #create cover letter using open ai and job description
-    #open a new window with cover letter returned by openAI and prompt user if theyd like to save to a file
-    def send():
-        prompt = "please write a personalized cover letter for this " + job_title + " at " + company_name + "\n Here is the job description: " + jobDesc + "\n And here is my resume: " + resume
-        outputtxt.insert(tkinter.END,sendData(prompt))
-    def save():
-        write_cover_letter_to_file(outputtxt.get("1.0",'end-1c'))
-        window.destroy()
-    
-    window = tkinter.Toplevel(GUIWindow)
-    window.title("Creating Cover Letter")
-    window.geometry("1280x720")
-
-    submitButton = tkinter.Button(window, text = "Create Cover Letter", command = send)
-    submitButton.pack()
-
-    outputlabel = tkinter.Label(window, text= "Cover Letter")
-    outputlabel.pack()
-
-    outputtxt = tkinter.Text(window, height=10, width=50)
-    outputtxt.pack()
-
-    saveCoverLetterButton = tkinter.Button(window, text = "Save Cover Letter", command = save)
-    saveCoverLetterButton.pack()
-    return
-
-def write_resume_to_file(data):
-    #write resume to a file
-    #if were saving new resume 
-    global newResume
-    newResume = data
-    return
-def write_cover_letter_to_file(data):
-    #write cover letter to a file
-    return
-
-def set_job_title():
-    tkinter.Label(GUIWindow, text=job_title).grid(row = 0, column = 1)
-def set_company_name():
-    tkinter.Label(GUIWindow, text=company_name).grid(row = 1, column = 1)
-def set_job_desc():
-    jobDesc_text = tkinter.Text(GUIWindow,height=10, width=50)
-    jobDesc_text.grid(row = 2, column = 1)
-    jobDesc_text.insert(tkinter.END,jobDesc)
 
 
 #application GUI
-GUIWindow = tkinter.Tk()
+GUIWindow = Tk()
 GUIWindow.geometry("1280x720")
+jobList = []
+
+def create_job():
+    #make a button to open the job window
+    #store job description for use in open ai commands
+    def destroy_window():
+        window.destroy()
+    def saveInfo():
+        jobDesc = jobdescriptiontxt.get("1.0",'end-1c')
+        job_title = e1.get()
+        company_name = e2.get()            
+        jobList.append(Job(jobDesc,job_title,company_name))
+        add_button(jobList[-1])
+        destroy_window()
+
+    window = Toplevel(GUIWindow)
+    window.title("Setting Job Description and Company Name")
+    window.geometry("1280x720")
+
+    Label(window, text='Job Title:').grid(row=0)
+    Label(window, text='Company Name').grid(row=1)
+    e1 = Entry(window)
+    e2 = Entry(window)
+    e1.grid(row=0, column=1)
+    e2.grid(row=1, column=1)
+
+    jobdescriptionlabel = Label(window, text= "Please paste your job description here:")
+    jobdescriptionlabel.grid(row = 2)
+    jobdescriptiontxt = Text(window, height=10, width=50)
+    jobdescriptiontxt.grid(row = 3)
+
+    submitButton = Button(window, text = "Save Information", command = saveInfo)
+    submitButton.grid(row = 4)
+    
+    return
+
+def add_button(job):
+    Button(jobFrame,text= job.job_title + " at " + job.company_name , command=job.open_window).grid(row = len(jobList),sticky = E)
+def upload_resume():
+    tf = filedialog.askopenfilename( 
+    title="resume_master.txt", 
+    filetypes=(("Text Files", "*.txt"),)
+    )
+    tf = open(tf)
+    global resume
+    resume = tf.read()
+    tf.close()
+
+#Create Existing Job Frame
+jobFrame = LabelFrame(GUIWindow,text = "Jobs", padx=15, pady=15)
+jobFrame.grid(row = 0)
+
+#Create New Job Button
+NewJobFrame = Frame(GUIWindow,padx=15, pady=15)
+NewJobFrame.grid(row = 1)
+Button(NewJobFrame, text="Create New Job", command = create_job).grid(row = 0)
+
+#upload resume button
+Button(NewJobFrame, text="Master Resume", command = upload_resume).grid(row = 1, sticky = E)
 
 
+"""
 #show current saved information
-tkinter.Label(GUIWindow, text='Job Title:').grid(row=0)
-tkinter.Label(GUIWindow, text='Company Name').grid(row=1)
-tkinter.Label(GUIWindow, text='Job Description').grid(row=2)
+Label(GUIWindow, text='Job Title:').grid(row=0)
+Label(GUIWindow, text='Company Name').grid(row=1)
+Label(GUIWindow, text='Job Description').grid(row=2)
 
 
 #define buttons
 
-setJobDescriptionButton = tkinter.Button(GUIWindow, text="Set Job Description", command = set_job_description)
+setJobDescriptionButton = Button(GUIWindow, text="Set Job Description", command = set_job_description)
 setJobDescriptionButton.grid(row = 3, column = 0)
 
 
-refineResumeButton = tkinter.Button(GUIWindow, text="Refine Resume", command = refine_resume)
+refineResumeButton = Button(GUIWindow, text="Refine Resume", command = refine_resume)
 refineResumeButton.grid(row = 3, column = 1)
 
-createCoverLetter = tkinter.Button(GUIWindow, text="Create Cover Letter", command = create_cover_letter)
+createCoverLetter = Button(GUIWindow, text="Create Cover Letter", command = create_cover_letter)
 createCoverLetter.grid(row = 3, column = 2)
+"""
 GUIWindow.mainloop()
--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 Use chatgpt to write customized cover letters based on job posting and resume
 
 TODO
+
+-Rework GUI so its more aesthetically pleasing
+
 -Write a page scraper to gather job description and job_title and company_name from a job posting URL
 
 -Allow the user to save their newly created resumes and cover letters
 
--Allow the user to define a master resume
+-Allow the user to define a master resume:
+    master resume naming scheme "resume_master"
